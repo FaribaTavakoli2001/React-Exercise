636acaf85debd9de813cb7615b6f3f021cebe8a7
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2023-09-03 17:15:43 +0330

    class-component

---
+++ src/Hi.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+
+class Hi extends Component {
+    render(){
+
+        return(
+            <>
+            <div>
+                <h3>Hello world</h3>
+            </div>
+            
+            </>
+        )
+    }
+
+}
+
+export default Hi ;
